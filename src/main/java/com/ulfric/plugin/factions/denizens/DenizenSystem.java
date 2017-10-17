5686f4a47f54f11deaa54c8f9101ee0f6508378a
Adam Harrison <ad@mharrison.us>
2017-10-17 13:36:11 +0300

    Update for changes in rethink-dragoon

--- src/main/java/com/ulfric/plugin/factions/denizens/DenizenSystem.java
+++ src/main/java/com/ulfric/plugin/factions/denizens/DenizenSystem.java
@@ -1,5 +1,6 @@
 package com.ulfric.plugin.factions.denizens;
 
+import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.dragoon.rethink.Database;
 import com.ulfric.dragoon.rethink.Store;
 import com.ulfric.plugin.entities.Entity;
@@ -7,6 +8,7 @@ import com.ulfric.plugin.entities.EntitySystem;
 
 public class DenizenSystem extends EntitySystem {
 
+	@Inject
 	@Database(table = "factions/denizens")
 	private Store<Entity> store;
 
--- src/main/java/com/ulfric/plugin/factions/factions/FactionSystem.java
+++ src/main/java/com/ulfric/plugin/factions/factions/FactionSystem.java
@@ -1,5 +1,6 @@
 package com.ulfric.plugin.factions.factions;
 
+import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.dragoon.rethink.Database;
 import com.ulfric.dragoon.rethink.Store;
 import com.ulfric.plugin.entities.Entity;
@@ -7,6 +8,7 @@ import com.ulfric.plugin.entities.EntitySystem;
 
 public class FactionSystem extends EntitySystem {
 
+	@Inject
 	@Database(table = "factions/factions")
 	private Store<Entity> store;
 
