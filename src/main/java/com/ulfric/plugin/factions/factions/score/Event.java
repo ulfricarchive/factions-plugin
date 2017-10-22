1d00161cf2ec1a581c9bc2664a51aced1e1b3ced
Adam Harrison <ad@mharrison.us>
2017-10-22 15:15:21 +0300

    Record date in score event

--- src/main/java/com/ulfric/plugin/factions/factions/score/Event.java
+++ src/main/java/com/ulfric/plugin/factions/factions/score/Event.java
@@ -1,5 +1,6 @@
 package com.ulfric.plugin.factions.factions.score;
 
+import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
 
@@ -8,6 +9,7 @@ import com.ulfric.commons.value.Bean;
 public class Event extends Bean { // TODO scores would really work better in something like a kafka db
 
 	private Integer score;
+	private Instant date;
 	private UUID target;
 	private Map<String, Object> details;
 
@@ -17,6 +19,14 @@ public class Event extends Bean { // TODO scores would really work better in som
 
 	public void setScore(Integer score) {
 		this.score = score;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
 	}
 
 	public UUID getTarget() {
