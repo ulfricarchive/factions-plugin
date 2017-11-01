86178a07f5e27cce3bcc9f602ff27bb1e24d3c2f
Adam Harrison <ad@mharrison.us>
2017-11-01 18:10:48 -0400

    Work on claims

--- src/main/java/com/ulfric/plugin/factions/factions/FactionSystem.java
+++ src/main/java/com/ulfric/plugin/factions/factions/FactionSystem.java
@@ -1,6 +1,9 @@
 package com.ulfric.plugin.factions.factions;
 
+import java.util.logging.Logger;
+
 import com.ulfric.dragoon.extension.inject.Inject;
+import com.ulfric.dragoon.extension.postconstruct.PostConstruct;
 import com.ulfric.dragoon.rethink.Database;
 import com.ulfric.dragoon.rethink.Store;
 import com.ulfric.plugin.entities.Entity;
@@ -12,9 +15,30 @@ public class FactionSystem extends EntitySystem {
 	@Database(table = "factions/factions")
 	private Store<Entity> store;
 
+	@Inject(optional = true)
+	private Logger logger;
+
+	@PostConstruct
+	public void initialize() {
+		store().listAllFromDatabase().thenAccept(list -> {
+			int size = list.size();
+			if (size == 0) {
+				log("No factions were loaded from the database");
+			} else {
+				log("Loaded " + size + " factions from the database");
+			}
+		});
+	}
+
 	@Override
 	protected Store<Entity> store() {
 		return store;
+	}
+
+	private void log(String message) {
+		if (logger != null) {
+			logger.info(message);
+		}
 	}
 
 }
--- src/main/java/com/ulfric/plugin/factions/factions/claims/Coordinate2d.java
+++ src/main/java/com/ulfric/plugin/factions/factions/claims/Coordinate2d.java
@@ -1,6 +1,7 @@
 package com.ulfric.plugin.factions.factions.claims;
 
 import com.ulfric.commons.value.Bean;
+import com.ulfric.spatialregions.shape.Point2d;
 
 public class Coordinate2d extends Bean {
 
@@ -21,6 +22,23 @@ public class Coordinate2d extends Bean {
 
 	public void setZ(Integer z) {
 		this.z = z;
+	}
+
+	public Point2d toPoint2d() {
+		return Point2d.builder()
+				.setX(x())
+				.setZ(z())
+				.build();
+	}
+
+	private int x() {
+		Integer x = getX();
+		return x == null ? 0 : x;
+	}
+
+	public int z() {
+		Integer z = getZ();
+		return z == null ? 0 : z;
 	}
 
 }
