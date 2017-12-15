6daff6d5c41e75d89b8c750cc6e110955980e038
Adam Harrison <ad@mharrison.us>
2017-12-15 18:38:49 +0000

    /f kick

--- src/main/java/com/ulfric/plugin/factions/command/FactionsKickCommand.java
+++ src/main/java/com/ulfric/plugin/factions/command/FactionsKickCommand.java
@@ -13,33 +13,56 @@ public class FactionsKickCommand extends TargetDenizenFactionFactionsCommand {
 
 	@Override
 	public void run() {
-		if (!faction.getIdentifier().equals(target.getFaction())) {
+		if (!isTargetInSameFaction()) {
 			tell("factions-kick-not-member");
 			return;
 		}
 
-		if (target.getIdentifier().equals(denizen.getIdentifier())) {
+		if (isSelfKick()) {
 			tell("factions-kick-self");
 			return;
 		}
 
-		int senderLevel = permissionLevel();
-		int targetLevel = calculatePermissionLevel(target);
-		if (targetLevel >= senderLevel) {
+		if (!isKickPermitted()) {
 			tell("factions-kick-rank-too-low");
 			return;
 		}
 
+		removeTargetFromFaction();
+		removeFactionFromTarget();
+
+		notifyInvolvedParties();
+	}
+
+	private boolean isTargetInSameFaction() {
+		return faction.getIdentifier().equals(target.getFaction());
+	}
+
+	private boolean isSelfKick() {
+		return target.getIdentifier().equals(denizen.getIdentifier());
+	}
+
+	private boolean isKickPermitted() {
+		int senderLevel = permissionLevel();
+		int targetLevel = calculatePermissionLevel(target);
+		return senderLevel > targetLevel;
+	}
+
+	private void removeTargetFromFaction() {
 		Map<UUID, Set<String>> roles = faction.getMembersToRoles();
 		if (roles != null) {
 			roles.remove(UniqueIdHelper.parseUniqueIdExact(target.getIdentifier()));
 			faction.setMembersToRoles(roles);
 			factions.persistFaction(faction);
 		}
+	}
 
+	private void removeFactionFromTarget() {
 		target.setFaction(null);
 		factions.persistDenizen(target);
+	}
 
+	private void notifyInvolvedParties() {
 		tellDenizensExceptForSender("factions-kick-by");
 		tell("factions-kick");
 		tellTarget("factions-kick-to");
